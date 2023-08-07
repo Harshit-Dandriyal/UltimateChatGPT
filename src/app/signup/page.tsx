0e90cb50ae0e3d20a04447f98d77e4c41687b33c
Harshit Dandriyal <56961488+Harshit-Dandriyal@users.noreply.github.com>
2023-08-07 14:56:11 +0530

    first commit

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function SignupPage() {
+    return (
+        <>
+        <div className="flex justify-center font-extrabold text-3xl">
+            Hello worldfdsf
+        </div>
+        </>
+    )
+}
